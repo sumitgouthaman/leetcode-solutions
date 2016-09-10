18dd48d357cbff3dcf67a6147ae1e36361b7924e
Sumit Gouthaman <sumitgt007@gmail.com>
2016-09-10 14:49:44 -0700

    InitialCommit

---
+++ src/LeetcodeSolutions/Solution1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SumitGouthaman.LeetcodeSolutions
+{
+    public class Solution1
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ test/LeetcodeSolutions.Tests/Solution1UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace SumitGouthaman.LeetcodeSolutions.UnitTests
+{
+    public class Solution1UnitTest
+    {
+        private readonly Solution1 _solution1;
+        public Solution1UnitTest()
+        {
+            _solution1 = new Solution1();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _solution1.IsPrime(1);
+            Assert.False(result, $"1 should not be prime");
+        }
+    }
+}
