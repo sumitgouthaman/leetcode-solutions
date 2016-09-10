abaf9bee4e3080116ed8376322be3d6584f90b57
Sumit Gouthaman <sumitgt007@gmail.com>
2016-09-10 15:35:26 -0700

    Added Solution 1

---
+++ src/LeetcodeSolutions/Sol001TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SumitGouthaman.LeetcodeSolutions
+{
+    public class Sol001TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums.Length < 2)
+                return new int[] { };
+
+            // Map from number => position
+            IDictionary<int, int> seenNumbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+
+                int position;
+                if (seenNumbers.TryGetValue(diff, out position))
+                {
+                    return new int[] { position, i };
+                }
+
+                if (!seenNumbers.ContainsKey(nums[i]))
+                    seenNumbers.Add(nums[i], i);
+            }
+
+            return new int[] { }; // No solution possible
+        }
+    }
+}
---
+++ test/LeetcodeSolutions.Tests/Sol001TwoSumTest.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace SumitGouthaman.LeetcodeSolutions.UnitTests
+{
+    public class Sol001TwoSumTest
+    {
+        private readonly Sol001TwoSum _solution;
+        public Sol001TwoSumTest()
+        {
+            _solution = new Sol001TwoSum();
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, 10)]
+        [InlineData(new int[] { 1 }, 10)]
+        public void TwoSum_InputArraySizeLessThan2_EmptyOutput(int[] input, int target)
+        {
+            int[] result = _solution.TwoSum(input, target);
+            Assert.Equal(0, result.Length);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 3 }, 10)]
+        [InlineData(new int[] { 5, 6 }, 10)]
+        [InlineData(new int[] { 1, 5, 6 }, 10)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 10)]
+        public void TwoSum_InputArrayHasTwoOrMoreElementsNoSolution_EmptyOutput(int[] input, int target)
+        {
+            int[] result = _solution.TwoSum(input, target);
+            Assert.Equal(0, result.Length);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 8 }, 10, new int[] { 0, 1 })]
+        [InlineData(new int[] { 5, 5 }, 10, new int[] { 0, 1 })]
+        [InlineData(new int[] { 1, 4, 6, 5 }, 10, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 4, 4, 6, 5 }, 10, new int[] { 1, 3 })]
+        [InlineData(new int[] { 1, 2, 4, 5, 7, 8 }, 10, new int[] { 1, 5 })]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })] // Example from Leetcode
+        public void TwoSum_InputArrayHasTwoOrMoreElementsHasSolution_CorrectOutput(int[] input, int target, int[] expectedResult)
+        {
+            int[] result = _solution.TwoSum(input, target);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
