80a1fde8771f078f23c2cff5414db31246816a4d
Sumit Gouthaman <sumitgt007@gmail.com>
2016-09-10 21:32:51 -0700

    Adding solution 3

---
+++ src/LeetcodeSolutions/Sol003.cs
@@ -0,0 +1,42 @@
+﻿// Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+using System.Collections.Generic;
+
+namespace SumitGouthaman.LeetcodeSolutions
+{
+    public class Sol003
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length < 2)
+                return s.Length;
+
+            IDictionary<char, int> charMap = new Dictionary<char, int>();
+            int maxLength = 0;
+            int leftMostWindowIndex = -1;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (!charMap.ContainsKey(c) || charMap[c] < leftMostWindowIndex)
+                {
+                    if (charMap.ContainsKey(c))
+                        charMap.Remove(c);
+
+                    charMap.Add(c, i);
+                    int newLength = i - leftMostWindowIndex;
+                    maxLength = (newLength > maxLength) ? newLength : maxLength;
+                }
+                else
+                {
+                    int charIndex = charMap[c];
+                    charMap.Remove(c);
+                    charMap.Add(c, i);
+                    leftMostWindowIndex = charIndex;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ test/LeetcodeSolutions.Tests/Sol003Test.1.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace SumitGouthaman.LeetcodeSolutions.UnitTests
+{
+    public class Sol003Test
+    {
+        private readonly Sol003 _solution;
+        public Sol003Test()
+        {
+            _solution = new Sol003();
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("a", 1)]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("tmmzuxt", 5)]
+        public void LengthOfLongestSubstring_VariousInputs_CorrectResult(string input, int expectedResult)
+        {
+            int result = _solution.LengthOfLongestSubstring(input);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
